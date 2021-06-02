7b6592272fcfd5e92347e2784c4f660ea0a829b2
sagittarius3008 <sagittarius30008@gmail.com>
2021-06-02 10:57:30 +0000

    Initialize repository

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '4778bf117568a804c86cd0b5b5a37c83f10f1a184870b5c61eef6193f3f26d990224364f2e0ec773c6acece7900cec531cf30ff2006951a8c24f8d0efe125012'
